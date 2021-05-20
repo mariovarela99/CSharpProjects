f5c6f6841de5d08df3f2c4b5fb840cf50f84469d
mariovarela99@outlook.com <mariovarela99@outlook.com>
2021-05-20 18:31:58 +0100

    :rocket: Despertador Aplication Nivel 3

---
+++ Projects/BrowserAplication/README.md
@@ -0,0 +1,2 @@
+# C-Projects
+Um Repositorio com Todos os Meus Projectos Feitos com C#
---
+++ Projects/DespertadorAplication/DespertadorAplication/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DespertadorAplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hora = int.Parse(textBox1.Text);
+            minuto = int.Parse(textBox2.Text);
+            timer1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (hora == DateTime.Now.Hour && minuto == DateTime.Now.Minute)
+            {
+                timer1.Enabled = false;
+                if (checkBox1.Checked == true)
+                {
+                    Microsoft.VisualBasic.Interaction.Shell(@"C:\Windows\System32\shutdown.exe -f -s");
+                }
+                else
+                {
+                    //MessageBox.Show("Alarme Tocado Com Sucesso.!");
+                    Microsoft.VisualBasic.Devices.Audio audio = new Microsoft.VisualBasic.Devices.Audio();
+                    audio.Play(openFileDialog1.FileName);
+                }
+            }
+        }
+
+        private void Campanhia_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+    }
+}
