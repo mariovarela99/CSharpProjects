5d514236199c62b9035138896f2c5166739f6221
mariovarela99@outlook.com <mariovarela99@outlook.com>
2021-05-20 18:49:53 +0100

    :rocket: Cronometro Aplication

---
+++ Projects/CronometroAplication/CronometroAplication/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CronometroAplication
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer t;
+        int h, m, s;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t = new System.Timers.Timer();
+            t.Interval = 1000; //1s
+            t.Elapsed += OnTimeEvent;
+        }
+
+        private void OnTimeEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                s += 1;
+                if (s == 60)
+                {
+                    s = 0;
+                    m += 1;
+                }
+                if (m == 60)
+                {
+                    m = 0;
+                    h += 1;
+                }
+                textBox1.Text = string.Format("{0}:{1}:{2}", h.ToString().PadLeft(2, '0'), m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0'));
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t.Stop();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            t.Stop();
+            Application.DoEvents();
+        }
+
+    }
+}
