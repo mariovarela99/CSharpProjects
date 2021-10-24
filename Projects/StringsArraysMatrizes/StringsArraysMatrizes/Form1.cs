7aaecf0493c73069444474da8bcfb63de614feb6
mariovarela99 <mariovarela99@outlook.com>
2021-10-24 01:15:19 +0100

    :sparkles:

---
+++ Projects/StringsArraysMatrizes/StringsArraysMatrizes/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace StringsArraysMatrizes
+{
+    public partial class Form1 : Form
+    {
+        public int[] numbers = new int[10];
+        public int verify = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+                numbers[i] = int.Parse(Interaction.InputBox(String.Format("Digite o Valor {0} => ", i)));
+           
+            foreach(int number in numbers)
+                lstNumbers.Items.Add(number);
+           
+            verify = int.Parse(Interaction.InputBox("Digite o valor a procurar => "));
+
+            if (Array.IndexOf(numbers, verify) >= 0)
+            {
+                MessageBox.Show(String.Format("O Number Esta no Vetor Na Position => {0} ", Array.IndexOf(numbers, verify)));
+                lstNumbers.Items.Clear();
+            }
+            else
+                MessageBox.Show("O Number Nao Esta no Vetor");
+        }
+    }
+}
---
+++ Projects/StringsArraysMatrizes/Tarefa1/Tarefa1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Tarefa1
+{
+    partial class Tarefa1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.Transparent;
+            this.btnIniciar.Font = new System.Drawing.Font("Almond Nougat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btnIniciar.Location = new System.Drawing.Point(322, 12);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(219, 29);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Entrar com os Dados";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 493);
+            this.Controls.Add(this.btnIniciar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+    }
+}
+
