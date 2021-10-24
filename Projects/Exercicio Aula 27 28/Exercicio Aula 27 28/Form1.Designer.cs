a2b2b29d1ce40f79fcdd3494696fefe40f5df4b7
mariovarela99 <mariovarela99@outlook.com>
2021-10-24 01:14:27 +0100

    :rocket:

---
+++ Projects/Exercicio Aula 27 28/Exercicio Aula 27 28/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Exercicio_Aula_27_28
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLer = new System.Windows.Forms.Button();
+            this.dgvValores = new System.Windows.Forms.DataGridView();
+            this.col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvValores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLer
+            // 
+            this.btnLer.Location = new System.Drawing.Point(176, 284);
+            this.btnLer.Name = "btnLer";
+            this.btnLer.Size = new System.Drawing.Size(124, 37);
+            this.btnLer.TabIndex = 0;
+            this.btnLer.Text = "Ler Valores";
+            this.btnLer.UseVisualStyleBackColor = true;
+            this.btnLer.Click += new System.EventHandler(this.btnLer_Click);
+            // 
+            // dgvValores
+            // 
+            this.dgvValores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvValores.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col1,
+            this.col2,
+            this.col3});
+            this.dgvValores.Location = new System.Drawing.Point(70, 94);
+            this.dgvValores.Name = "dgvValores";
+            this.dgvValores.Size = new System.Drawing.Size(347, 170);
+            this.dgvValores.TabIndex = 1;
+            // 
+            // col1
+            // 
+            this.col1.HeaderText = "Coluna 0";
+            this.col1.Name = "col1";
+            // 
+            // col2
+            // 
+            this.col2.HeaderText = "Coluna 2";
+            this.col2.Name = "col2";
+            // 
+            // col3
+            // 
+            this.col3.HeaderText = "Coluna 3";
+            this.col3.Name = "col3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 333);
+            this.Controls.Add(this.dgvValores);
+            this.Controls.Add(this.btnLer);
+            this.Name = "Form1";
+            this.Text = "Exercicio Aula 27 28";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvValores)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLer;
+        private System.Windows.Forms.DataGridView dgvValores;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col3;
+    }
+}
+
---
+++ Projects/Exercicio Aula 27 28/Exercicio Aula 27 28/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Exercicio_Aula_27_28
+{
+    public partial class Form1 : Form
+    {
+        static string path = @"C:\Users\mariovarela99\3D Objects\exercise\file.txt";
+        StreamReader ReadFile;
+        string[,] matriz = new string[4, 3];
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnLer_Click(object sender, EventArgs e)
+        {
+            int k = 0;
+            char virgula = ',';
+            string[] Valores = new string[12];
+            dgvValores.Rows.Add(3);
+            ReadFile = new StreamReader(path);
+
+            string Text = ReadFile.ReadLine();
+            Valores = Text.Split(virgula);
+            ReadFile.Close();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matriz[i, j] = Valores[k];
+                    k++;
+                }
+            }
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    dgvValores[j, i].Value = matriz[i, j];
+                }
+            }
+
+            MessageBox.Show("Dados impressos com sucesso.!");
+        }
+    }
+}
