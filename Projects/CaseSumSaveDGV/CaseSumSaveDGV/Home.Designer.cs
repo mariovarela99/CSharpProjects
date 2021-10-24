55bd136795e5d62fd4f2734f7615338793c63a55
mariovarela99 <mariovarela99@outlook.com>
2021-10-24 01:13:03 +0100

    :rocket: :sparkles: Enviando projecto de somar dois numbers de guardar num ficheiro e depois ler e mostrar num DGV

---
+++ Projects/CaseSumSaveDGV/CaseSumSaveDGV/Home.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace CaseSumSaveDGV
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDo = new System.Windows.Forms.Button();
+            this.number2 = new System.Windows.Forms.TextBox();
+            this.number1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.n1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.n2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.result = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDo
+            // 
+            this.btnDo.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDo.Location = new System.Drawing.Point(461, 12);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(110, 36);
+            this.btnDo.TabIndex = 0;
+            this.btnDo.Text = "Somar e Salvar";
+            this.btnDo.UseVisualStyleBackColor = true;
+            this.btnDo.Click += new System.EventHandler(this.btnDo_Click);
+            // 
+            // number2
+            // 
+            this.number2.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number2.Location = new System.Drawing.Point(233, 28);
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(222, 20);
+            this.number2.TabIndex = 1;
+            // 
+            // number1
+            // 
+            this.number1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number1.Location = new System.Drawing.Point(12, 28);
+            this.number1.Name = "number1";
+            this.number1.Size = new System.Drawing.Size(215, 20);
+            this.number1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(230, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number 2";
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.n1,
+            this.n2,
+            this.result});
+            this.dgvDados.Location = new System.Drawing.Point(15, 96);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.Size = new System.Drawing.Size(556, 223);
+            this.dgvDados.TabIndex = 5;
+            // 
+            // n1
+            // 
+            this.n1.HeaderText = "Number 1";
+            this.n1.Name = "n1";
+            this.n1.ReadOnly = true;
+            this.n1.Width = 150;
+            // 
+            // n2
+            // 
+            this.n2.HeaderText = "Number 2";
+            this.n2.Name = "n2";
+            this.n2.ReadOnly = true;
+            this.n2.Width = 150;
+            // 
+            // result
+            // 
+            this.result.HeaderText = "Resultado da Soma";
+            this.result.Name = "result";
+            this.result.ReadOnly = true;
+            this.result.Width = 200;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Dados do Arquivo.!";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(233, 327);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(110, 25);
+            this.btnRefresh.TabIndex = 7;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 364);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.number1);
+            this.Controls.Add(this.number2);
+            this.Controls.Add(this.btnDo);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Home_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDo;
+        private System.Windows.Forms.TextBox number2;
+        private System.Windows.Forms.TextBox number1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.DataGridViewTextBoxColumn n1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn n2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn result;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ Projects/CaseSumSaveDGV/CaseSumSaveDGV/Home.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CaseSumSaveDGV
+{
+    public partial class Home : Form
+    {
+        public static string FilePath = @"C:\Users\mariovarela99\Documents\Test\Testing.txt";
+        string[] Lines;
+
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            WriteOnDGV();
+        }
+
+        private void btnDo_Click(object sender, EventArgs e)
+        {
+            int n1 = int.Parse(number1.Text);
+            int n2 = int.Parse(number2.Text);
+            int soma = n1 + n2;
+
+            File.AppendAllText(FilePath, String.Format("{0}-{1}-{2}\n", n1, n2, soma));
+            CleanDGV();
+            WriteOnDGV();
+
+        }
+
+        public void WriteOnDGV()
+        {
+            CleanDGV();
+
+            Lines = File.ReadAllLines(FilePath);
+
+            dgvDados.Rows.Add(Lines.Length);
+
+            for (int i = 0; i <= Lines.Length - 1; i++)
+            {
+                string[] numbers = Lines[i].Split('-');
+                dgvDados[0, i].Value = numbers[0];
+                dgvDados[1, i].Value = numbers[1];
+                dgvDados[2, i].Value = numbers[2];
+            }
+        }
+
+        private void CleanDGV()
+        {
+            dgvDados.Rows.Clear();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            WriteOnDGV();
+        }
+    }
+}
