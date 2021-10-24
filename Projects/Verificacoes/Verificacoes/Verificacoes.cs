0d6e74aa208bfb3a9eca95e27bbbb4563c1dfceb
mariovarela99 <mariovarela99@outlook.com>
2021-10-24 01:15:55 +0100

    :sparkles:

---
+++ Projects/Verificacoes/Verificacoes/Verificacoes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verificacoes
+{
+    class Verificacoes
+    {
+        public int quantDivsaores(int num) 
+        {
+            int cont = 0;
+
+            for (int i = 0; i < num; i++ )
+
+
+                return 1;
+        }
+    }
+}
